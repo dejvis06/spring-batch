db69db75138c0c55a7dca7ccc48d0af13a014c81
beqirajd <dejvis.beqiraj@linksmt.it>
2023-08-05 15:43:20 +0200

    create mapper class

---
+++ src/main/java/com/example/core/services/Mapper.java
@@ -0,0 +1,55 @@
+package com.example.core.services;
+
+import com.example.core.domain.models.taxonomic.TaxonomicInput;
+import com.example.core.domain.models.taxonomic.TaxonomicTarget;
+import com.example.rest.dto.JobExecutionDTO;
+import com.example.rest.dto.job.InputData;
+import com.example.rest.dto.job.TargetData;
+import org.springframework.batch.core.JobExecution;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Mapper {
+
+    public static JobExecutionDTO buildJobExecutionDTO(JobExecution type) {
+        JobExecution jobExecution = type;
+
+        LocalDateTime startTime = null;
+        if (jobExecution.getStartTime() != null) {
+            startTime = LocalDateTime.ofInstant(jobExecution.getStartTime().toInstant(),
+                    ZoneId.systemDefault());
+        }
+        LocalDateTime endTime = null;
+        if (jobExecution.getEndTime() != null) {
+            endTime = LocalDateTime.ofInstant(jobExecution.getEndTime().toInstant(),
+                    ZoneId.systemDefault());
+        }
+
+        return JobExecutionDTO.builder()
+                .id(jobExecution.getId())
+                .fileName(type.getJobInstance().getJobName())
+                .status(jobExecution.getStatus().toString())
+                .startTime(startTime)
+                .endTime(endTime)
+                .build();
+    }
+
+    public static TaxonomicInput buildTaxonomicInput(InputData inputData) {
+        return TaxonomicInput.builder()
+                .dataset(inputData.getDataset())
+                .csv(inputData.getCsv())
+                .rowDelimiter(inputData.getRowDelimiter())
+                .columnDelimiter(inputData.getColumnDelimiter())
+                .build();
+    }
+
+    public static TaxonomicTarget buildTaxonomicTarget(TargetData targetData) {
+        return TaxonomicTarget.builder()
+                .domain(targetData.getDomain())
+                .environment(targetData.getEnvironment())
+                .taxonomicBackbone(targetData.getTaxonomicBackbone())
+                .rules(targetData.getRules())
+                .build();
+    }
+}
