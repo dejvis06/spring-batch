ec8c62d02098f339062f55d9326145c22263b4db
beqirajd <dejvis.beqiraj@linksmt.it>
2023-08-05 14:31:48 +0200

    create custom exception

---
+++ src/main/java/com/example/common/exceptions/TypeNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.common.exceptions;
+
+public class TypeNotFoundException extends Exception {
+    public TypeNotFoundException(String message) {
+        super(message);
+    }
+}
