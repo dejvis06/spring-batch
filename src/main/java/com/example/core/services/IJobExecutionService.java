f7333c63fe50569762df10373096ea558b847d1c
beqirajd <dejvis.beqiraj@linksmt.it>
2023-08-06 16:17:53 +0200

    add id to params

--- src/main/java/com/example/core/services/IJobExecutionService.java
+++ src/main/java/com/example/core/services/IJobExecutionService.java
@@ -4,6 +4,7 @@ import com.example.common.exceptions.TypeNotFoundException;
 import com.example.core.domain.models.taxonomic.TaxonomicInput;
 import com.example.core.domain.models.taxonomic.TaxonomicTarget;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.configuration.DuplicateJobException;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
@@ -12,7 +13,7 @@ import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface IJobExecutionService {
-    JobExecution save(String jobName, TaxonomicInput inputData, TaxonomicTarget targetData) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException;
+    JobExecution save(String jobName, TaxonomicInput inputData, TaxonomicTarget targetData, Long id) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, TypeNotFoundException, DuplicateJobException;
 
     List<JobExecution> findAll(Pageable pageable) throws TypeNotFoundException;
 }
