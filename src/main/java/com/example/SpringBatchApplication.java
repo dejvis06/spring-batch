711b897af424c124927551044f974e94a69c2c3e
beqirajd <dejvis.beqiraj@linksmt.it>
2023-08-07 11:51:56 +0200

    add openapi descriptions

--- src/main/java/com/example/SpringBatchApplication.java
+++ src/main/java/com/example/SpringBatchApplication.java
@@ -1,13 +1,25 @@
 package com.example;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBatchApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBatchApplication.class, args);
+    }
 
+    @Bean
+    public OpenAPI openAPIdefinition() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Taxonomic Job Management")
+                        .version("demo")
+                        .description("Spring Batch Demo project for Taxonomic Job Management"));
+    }
 }
